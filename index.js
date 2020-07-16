9d8ad087443835a2c551fc88852db8396d05b624
Nick Chi <nick.chi@gmail.com>
2020-07-16 21:17:20 +0800

    add start script

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const Koa = require('koa');
 const app = new Koa();
-var port = process.env.port||3000;
+var port = process.env.port || 3000;
 
-app.use(ctx => {
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+app.use(async (ctx) => {
     ctx.body = 'Hello Koa';
 });
 
-app.listen(process.env.port||3000, () => {
+app.listen(process.env.port || 3000, () => {
     console.log(`Server started on port`);
 });
+
+module.exports = app.callback();
